9a070c57b11edb7c74fb472a07666c8b43c533c0
puxhaineblak <albertanodal@gmail.com>
2024-11-20 16:08:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,1 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-from Bio import SeqIO
-import matplotlib.pyplot as plt
-import plotly.express as px
-import py3Dmol 
-from io import StringIO
-
+pip install streamlit pandas numpy biopython matplotlib plotly py3Dmol
