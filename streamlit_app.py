7f97517eddb8b46c8ae2d7b872af2575a3dbe490
puxhaineblak <albertanodal@gmail.com>
2024-11-20 16:12:14 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
-pip install streamlit pandas numpy biopython matplotlib plotly py3Dmol
+pip install streamlit 
+streamlit
+pandas
+numpy
+biopython
+matplotlib
+plotly
+py3Dmol
+pip install -r requirements.txt
